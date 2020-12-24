748ca1c53ec7bf960d389454dcace561d026dfa7
qmya <qandeelgul90@gmail.com>
2020-12-24 17:01:45 -0500

    created a constructor person

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//Creating a constructor fuction
+//constrctor first letter will be capital
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991); //calling constructor we use new keyword
+console.log(jonas);
+//WHAT DOES NEW KEYWORD DO?
+//1. NEW {} IS CREATED
+//2. FUNCTION IS CALLED, "THIS" KEYWORD POINTS TO NEW {}
+//3.{} LINKED TO PROTOTYPE
+//4.FUNCTION AUTOMATICALLY RETURN {}
